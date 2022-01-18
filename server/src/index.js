ef7efa6ea65c28ea486da829176b1fa6219c525e
manuel-heav <manuelheav32@gmail.com>
2022-01-18 10:52:51 -0500

    set up error middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,28 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found = ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🧁' : error.stack,
+  });
+});
+
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
