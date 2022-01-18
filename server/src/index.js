e2c0a378489704d7d9ff106a65a4a47d85c7a73d
manuel-heav <manuelheav32@gmail.com>
2022-01-18 10:37:30 -0500

    set up the express app

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
-
-
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Listening to port ${port}`);
+});
