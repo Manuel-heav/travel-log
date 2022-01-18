cffcae408f71b9bc448dbed6b31999642313028d
manuel-heav <manuelheav32@gmail.com>
2022-01-18 10:12:29 -0500

    set up eslint

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express();
+
+
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+})
