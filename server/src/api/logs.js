86fe1e15e5d5f10ca6864fde9d27e803c74c9731
manuel-heav <manuelheav32@gmail.com>
2022-01-24 11:09:44 -0500

    just excluded some important files

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,10 +4,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'works',
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post('/', async (req, res, next) => {
